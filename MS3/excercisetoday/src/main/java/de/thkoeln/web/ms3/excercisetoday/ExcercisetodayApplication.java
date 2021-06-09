bfad8d2e9d0a96f3604df69011ca36c1c7d01c73
Fabian Tsirogiannis <fabian_pascal.tsirogiannis@smail.th-koeln.de>
2021-06-09 08:06:41 +0200

    initial commit

---
+++ MS3/excercisetoday/src/main/java/de/thkoeln/web/ms3/excercisetoday/ExcercisetodayApplication.java
@@ -0,0 +1,11 @@
+package de.thkoeln.web.ms3.excercisetoday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcercisetodayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExcercisetodayApplication.class, args);
+	}
+}
---
+++ MS3/excercisetoday/src/test/java/de/thkoeln/web/ms3/excercisetoday/ExcercisetodayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thkoeln.web.ms3.excercisetoday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcercisetodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
