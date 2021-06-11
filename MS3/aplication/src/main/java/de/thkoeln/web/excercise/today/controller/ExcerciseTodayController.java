d8baa970f01e20c45407ae11e1e4ec3c7fc2cb43
Fabian Tsirogiannis <fabian_pascal.tsirogiannis@smail.th-koeln.de>
2021-06-11 09:16:25 +0200

    Website rendered by Spring+

--- MS3/aplication/src/main/java/de/thkoeln/web/excercise/today/controller/ExcerciseTodayController.java
+++ MS3/aplication/src/main/java/de/thkoeln/web/excercise/today/controller/ExcerciseTodayController.java
@@ -3,15 +3,32 @@ package de.thkoeln.web.excercise.today.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ExcerciseTodayController {
     @GetMapping("/")
     public String home() {
-        return "WEB_excercise_today_Home";
+        return "index";
     }
     @GetMapping("/upload")
-    public String upload(Model model) {
-        return "WEB_excercise_today_Upload";
+    public String upload() {
+        return "upload";
+    }
+    @GetMapping("/posts")
+    public String posts() {
+        return "posts";
+    }
+    @GetMapping("/post1")
+    public String post1() {
+        return "post1";
+    }
+    @GetMapping("/post2")
+    public String post2() {
+        return "post2";
+    }
+    @GetMapping("/post3")
+    public String post3() {
+        return "post3";
     }
 }
