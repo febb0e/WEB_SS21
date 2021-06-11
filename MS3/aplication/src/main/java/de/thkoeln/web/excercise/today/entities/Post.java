da87e4c7762f93dd58a9cef2c2848b77c96c39cd
Fabian Tsirogiannis <fabian_pascal.tsirogiannis@smail.th-koeln.de>
2021-06-11 17:12:51 +0200

    Controller implemented + Preparation of GET/POST Request uploads

--- MS3/aplication/src/main/java/de/thkoeln/web/excercise/today/entities/Post.java
+++ MS3/aplication/src/main/java/de/thkoeln/web/excercise/today/entities/Post.java
@@ -14,25 +14,33 @@ public class Post {
     private String date;
     private String title;
     private String description;
+    private String image;
 
-    public Post(String date, String title, String description) {
-        this.date = date;
-        this.title = title;
-        this.description = description;
+    public Post() {
     }
 
     public Long getId() {return id;}
-
+    public void setId() {
+        this.id = id;
+    }
     public String getDate() {
         return date;
     }
-
+    public void setDate() {
+        this.date = date;
+    }
     public String getTitle() {
         return title;
     }
-
-    public String getDescription() {
-        return description;
+    public void setTitle() {
+        this.title = title;
     }
-
+    public String getDescription() { return description; }
+    public void setDescription() {
+        this.description = description;
+    }
+    public String getImage() { return image; }
+    public void setImage() {
+        this.image = image;
+    }
 }
